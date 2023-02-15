59f8b62cde63f1c2de0d26d27be64f0ede576874
Henry Lay <h.lay@lancaster.ac.uk>
2023-02-15 14:30:04 -0600

    Create particle ancestry object

---
+++ lardataobj/Simulation/ParticleAncestryMap.cxx
@@ -0,0 +1,51 @@
+#ifndef PARTICLEANCESTRYMAP_CXX
+#define PARTICLEANCESTRYMAP_CXX
+
+#include "lardataobj/Simulation/ParticleAncestryMap.h"
+
+namespace sim {
+
+  void ParticleAncestryMap::SetMap(const std::map<int, std::set<int>> &map)
+  {
+    fParticleMap = map;
+  }
+
+  std::map<int, std::set<int>> ParticleAncestryMap::GetMap() const
+  {
+    return fParticleMap;
+  }
+
+  bool ParticleAncestryMap::HasDroppedDescendants(const int &trackid)
+  {
+    return fParticleMap.count(trackid) != 0;
+  }
+  
+  std::set<int> ParticleAncestryMap::GetAllDroppedDescendants(const int &trackid)
+  {
+    return fParticleMap[trackid];
+  }
+
+  bool ParticleAncestryMap::HasAncestor(const int &trackid)
+  {
+    for(auto const& [ancestor, descendants] : fParticleMap)
+      {
+        if(descendants.count(trackid) != 0)
+          return true;
+      }
+
+    return false;
+  }
+
+  int ParticleAncestryMap::GetAncestor(const int &trackid)
+  {
+    for(auto const& [ancestor, descendants] : fParticleMap)
+      {
+        if(descendants.count(trackid) != 0)
+          return ancestor;
+      }
+
+    return -std::numeric_limits<int>::max();
+  }
+}
+
+#endif
---
+++ lardataobj/Simulation/ParticleAncestryMap.h
@@ -0,0 +1,45 @@
+/** 
+ * \class ParticleAncestryMap
+ *
+ * \brief Product to store the mapping of track IDs that were
+ *        dropped in largeant back to their ancestor that wasn't
+ *        dropped.
+ *
+ * \author Henry Lay (h.lay@lancaster.ac.uk)
+ *
+ */
+
+#ifndef PARTICLEANCESTRYMAP_H
+#define PARTICLEANCESTRYMAP_H
+
+#include <map>
+#include <set>
+
+namespace sim {
+
+  class ParticleAncestryMap {
+
+    std::map<int, std::set<int>> fParticleMap;
+
+  public:
+
+    ParticleAncestryMap() {}
+    
+    ParticleAncestryMap(const std::map<int, std::set<int>> &particleMap)
+      : fParticleMap(particleMap)
+    {}
+
+    void SetMap(const std::map<int, std::set<int>> &map);
+
+    std::map<int, std::set<int>> GetMap() const;
+
+    bool HasDroppedDescendants(const int &trackid);
+
+    std::set<int> GetAllDroppedDescendants(const int &trackid);
+
+    bool HasAncestor(const int &trackid);
+    
+    int GetAncestor(const int &trackid);
+  };
+}
+#endif
