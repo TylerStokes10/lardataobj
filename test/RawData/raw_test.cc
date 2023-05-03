e7051db4d710a8b7311bcba2d1275e5397446926
Lynn Garren <garren@fnal.gov>
2023-05-03 15:46:47 -0500

    and also test_info

--- test/RawData/raw_test.cc
+++ test/RawData/raw_test.cc
@@ -244,7 +244,7 @@ void RunDataCompressionTests(DataCreatorBase* pDataCreator)
     const std::vector<short> data(pDataCreator->create(size_info.first));
 
     // test the same data with different compression algorithms:
-    for (const auto test_info : CompressionModes) {
+    for (const auto &test_info : CompressionModes) {
       // a "nice" label to make output vaguely meaningful...
       std::string test_id =
         pDataCreator->name() + " (" + size_info.second + " " + test_info.second + ")";
