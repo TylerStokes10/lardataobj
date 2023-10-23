d718817306d5ac530f2e3be9ced15d1d16ef0dd5
Tingjun Yang <tjyang@fnal.gov>
2023-10-23 12:08:44 -0500

    Add IO rule to convert time stamp in the old version of recob::OpWaveform.

--- lardataobj/RecoBase/classes.h
+++ lardataobj/RecoBase/classes.h
@@ -2,9 +2,11 @@
 #include "canvas/Persistency/Common/PtrVector.h"
 #include "canvas/Persistency/Common/Wrapper.h"
 
+#include "TTimeStamp.h"
 #include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
 
 #include "lardataobj/RawData/RawDigit.h"
+#include "lardataobj/RawData/RDTimeStamp.h"
 #include "lardataobj/RecoBase/Cluster.h"
 #include "lardataobj/RecoBase/Edge.h"
 #include "lardataobj/RecoBase/EndPoint2D.h"
